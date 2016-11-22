9748e94958e87bbb398894d998abd11885ac7411
liangchengcheng <1038127753@qq.com>
2016-11-22 08:26:55 +0800

    add util class file
    add util class file

---
+++ src/main/java/com/lcc/util/PasswordUtil.java
@@ -0,0 +1,30 @@
+package com.lcc.util;
+
+import java.util.Date;
+import java.util.Random;
+/**
+ * Created by lcc on 2016/11/22.
+ */
+public class PasswordUtil {
+    public final static String[] _NUM = { "2", "3", "4", "5", "6", "7", "8", "9" };
+
+    public final static String[] _WORD = { "a", "b", "c", "d", "e", "f", "g", "h", "j", "k", "m", "n", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "A",
+            "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "~", "!", "@", "#", "$", "%", "^",
+            "&", "+", "?" };
+
+    public static String randomPassword() {
+        StringBuffer password = new StringBuffer();
+        Random random = new Random(new Date().getTime());
+        Boolean flag = false;
+        int length = random.nextInt(3) + 8;
+        for (int i = 0; i < length; i++) {
+            if (flag) {
+                password.append(_NUM[random.nextInt(_NUM.length)]);
+            } else {
+                password.append(_WORD[random.nextInt(_WORD.length)]);
+            }
+            flag = !flag;
+        }
+        return password.toString();
+    }
+}
